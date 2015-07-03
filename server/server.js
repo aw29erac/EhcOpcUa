229c248f832ebf2a603de65bb5b4966dadaa0dcd
jobauer <jobauer@ehome09>
2015-07-03 14:42:40 +0200

    working example

--- server/server.js
+++ server/server.js
@@ -17,63 +17,63 @@ function post_initialize() {
     function construct_my_address_space(server) {
     
         // declare some folders
-         server.engine.addFolder("RootFolder",{ browseName: "MyDevice"});
-    
-        // add variables in folders
-        // add a variable named MyVariable1 to the newly created folder "MyDevice"
-        var variable1 = 1;
-        
-        // emulate variable1 changing every 500 ms
-        setInterval(function(){  variable1+=1; }, 500);
-        
-        server.nodeVariable1 = server.engine.addVariable("MyDevice",{
-                browseName: "MyVariable1",
-                dataType: "Double",
-                value: {
-                    get: function () {
-                        return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
-                    }
-                }
-        });
+//        server.engine.addFolder("RootFolder",{ browseName: "MyDevice"});
+//    
+//        // add variables in folders
+//        // add a variable named MyVariable1 to the newly created folder "MyDevice"
+//        var variable1 = 1;
+//        
+//        // emulate variable1 changing every 500 ms
+//        setInterval(function(){  variable1+=1; }, 500);
+//        
+//        server.nodeVariable1 = server.engine.addVariable("MyDevice",{
+//                browseName: "MyVariable1",
+//                dataType: "Double",
+//                value: {
+//                    get: function () {
+//                        return new opcua.Variant({dataType: opcua.DataType.Double, value: variable1 });
+//                    }
+//                }
+//        });
         
         
         // add a variable named MyVariable2 to the newly created folder "MyDevice"
-        var variable2 = 10.0;
-        server.nodeVariable2 = server.engine.addVariable("MyDevice",{
+//        var variable2 = 10.0;
+//        server.nodeVariable2 = server.engine.addVariable("MyDevice",{
+//        
+//            nodeId: "ns=4;b=1020FFAA", // some opaque NodeId in namespace 4
+//            browseName: "MyVariable2",
+//            dataType: "Double",    
+//            value: {
+//                get: function () {
+//                    return new opcua.Variant({dataType: opcua.DataType.Double, value: variable2 });
+//                },
+//                set: function (variant) {
+//                    variable2 = parseFloat(variant.value);
+//                    return opcua.StatusCodes.Good;
+//                }
+//            }
+//        });
         
-            nodeId: "ns=4;b=1020FFAA", // some opaque NodeId in namespace 4
-            browseName: "MyVariable2",
-            dataType: "Double",    
-            value: {
-                get: function () {
-                    return new opcua.Variant({dataType: opcua.DataType.Double, value: variable2 });
-                },
-                set: function (variant) {
-                    variable2 = parseFloat(variant.value);
-                    return opcua.StatusCodes.Good;
-                }
-            }
-        });
+//        var os = require("os");
+//        /**
+//         * returns the percentage of free memory on the running machine
+//         * @return {double}
+//         */
+//        function available_memory() {
+//            // var value = process.memoryUsage().heapUsed / 1000000;
+//            var percentageMemUsed = os.freemem() / os.totalmem() * 100.0;
+//            return percentageMemUsed;
+//        }
         
-        var os = require("os");
-        /**
-         * returns the percentage of free memory on the running machine
-         * @return {double}
-         */
-        function available_memory() {
-            // var value = process.memoryUsage().heapUsed / 1000000;
-            var percentageMemUsed = os.freemem() / os.totalmem() * 100.0;
-            return percentageMemUsed;
-        }
-        
-        server.nodeVariable3 = server.engine.addVariable("MyDevice", {
-            nodeId: "ns=4;s=free_memory", // a string nodeID
-            browseName: "FreeMemory",
-            dataType: "Double",    
-            value: {
-                get: function () {return new opcua.Variant({dataType: opcua.DataType.Double, value: available_memory() });}
-            }
-        });
+//        server.nodeVariable3 = server.engine.addVariable("MyDevice", {
+//            nodeId: "ns=4;s=free_memory", // a string nodeID
+//            browseName: "FreeMemory",
+//            dataType: "Double",    
+//            value: {
+//                get: function () {return new opcua.Variant({dataType: opcua.DataType.Double, value: available_memory() });}
+//            }
+//        });
     
     }
     
